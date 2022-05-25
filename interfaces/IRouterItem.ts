a9d10deb6716839dc3827920db282910d10fe21f
nano1709 <ignaciovargasrojas@hotmail.com>
2022-05-24 22:36:55 -0600

    Change landing page

---
+++ interfaces/IRouterItem.ts
@@ -0,0 +1,5 @@
+export interface IRouterItem{
+    id: number, 
+    pageFileName: string, 
+    textToDisplay: string
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,34 @@
 <template>
-  <Tutorial/>
+  <main>
+    <h1>Nacho home page</h1>
+
+    <ul>
+        <li v-for="item in items" :key="item.id">
+          <NuxtLink :to="item.pageFileName">
+            {{ item.textToDisplay }}
+          </NuxtLink>
+        </li>
+    </ul>
+  </main>
 </template>
 
-<!-- <script lang="ts">
-import Vue from 'vue'
+<script>
+import Vue from 'vue';
 
+// TODO - figure out how to implement interfaces for type safety (IRouterItem)
+// TODO - styling
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data(){
+    return {
+      items: [
+        { id: 1, pageFileName: 'Tutorial', textToDisplay: 'Go to tutorial' },
+        { id: 2, pageFileName: 'Blog voting page', textToDisplay: 'Blog voting page' },
+        { id: 3, pageFileName: 'Toggle switch page', textToDisplay: 'Toggle switch page' },
+        { id: 4, pageFileName: 'TODO', textToDisplay: 'TODO' },
+      ]     
+    }
+  }, 
 })
-</script> -->
+
+</script>
